5ce917c1457a2dba71e8a90058834c4c0a3c5207
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2025-05-05 14:53:15 +0500

    dtos for auth

--- src/main/java/com/abbos/moviego/dto/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package com.abbos.moviego.dto;
-
-/**
- * @author Aliabbos Ashurov
- * @version 1.0
- * @since 2025-05-02
- */
-public class LoginDto {
-}
---
+++ src/main/java/com/abbos/moviego/dto/auth/LoginDto.java
@@ -0,0 +1,22 @@
+package com.abbos.moviego.dto.auth;
+
+import com.abbos.moviego.dto.base.Request;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+/**
+ * @author Aliabbos Ashurov
+ * @version 1.0
+ * @since 2025-05-02
+ */
+public record LoginDto(
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
+        String email,
+
+        @NotBlank(message = "Password is required")
+        @Size(min = 8, message = "Password must be at least 8 characters")
+        String password
+) implements Request {
+}
