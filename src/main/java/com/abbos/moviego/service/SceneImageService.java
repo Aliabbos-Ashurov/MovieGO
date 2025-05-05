ce3b5e12545947a4d4f1e09a605da2f267300e84
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2025-05-05 14:55:11 +0500

    Added Services

---
+++ src/main/java/com/abbos/moviego/service/SceneImageService.java
@@ -0,0 +1,9 @@
+package com.abbos.moviego.service;
+
+/**
+ * @author Aliabbos Ashurov
+ * @version 1.0
+ * @since 2025-05-05
+ */
+public class SceneImageService {
+}
---
+++ src/main/java/com/abbos/moviego/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.abbos.moviego.service.impl;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Aliabbos Ashurov
+ * @version 1.0
+ * @since 2025-05-05
+ */
+@Service
+public class UserServiceImpl {
+    // TODO: start services sides!
+}
